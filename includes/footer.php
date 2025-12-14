858174bf0d9b7bcbc983e829f87ea18f6e2c880b
Hikam666 <aklilhikam6@gmail.com>
2025-12-14 14:02:04 +0700

    hikam

--- includes/footer.php
+++ includes/footer.php
@@ -28,7 +28,7 @@
                             <a class="btn btn-link" href="<?php echo SITE_URL; ?>/public/fasilitas.php">Fasilitas</a>
                             <a class="btn btn-link" href="<?php echo SITE_URL; ?>/public/publikasi.php">Publikasi</a>
                             <a class="btn btn-link" href="<?php echo SITE_URL; ?>/public/berita.php">Berita & Pengumuman</a>
-                            <a class="btn btn-link" href="<?php echo SITE_URL; ?>/public/galeri.php">Galeri Kegiatan</a>
+                            <a class="btn btn-link" href="<?php echo SITE_URL; ?>/public/galeri.php">Galeri</a>
                             <a class="btn btn-link" href="<?php echo SITE_URL; ?>/public/kontak.php">Kontak & Kerja Sama</a>
                         </div>
                     </div>
